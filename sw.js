b4713d9b8c82ffa19665c5976acf7b4f6c698ba5
cursoportuguesyespanol-dot <cursoportuguesyespanol@gmail.com>
2026-04-02 15:54:25 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'duals-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.pathname.startsWith('/wa')) return; // no cachear el proxy
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
+});
